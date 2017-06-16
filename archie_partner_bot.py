8fc02f3a103d6b15d60272cfbcc7fac4e2b4a3dd
alex08neo <alex081994@outlook.com>
2017-06-16 12:04:18 -0400

    add taxi command with id add new command source for seek source of picture

--- archie_partner_bot.py
+++ archie_partner_bot.py
@@ -8,6 +8,8 @@ logging.basicConfig(format="%(asctime)s-%(name)s-%(levelname)s-%(message)s")
 with open('_token', 'r') as rf:
     _token = rf.read()
 updater = Updater(token=_token)
-updater.dispatcher.add_handler(commands.start_handler)
-updater.dispatcher.add_handler(commands.uppercase_handler)
+dispatcher = updater.dispatcher
+
+for handler in commands.COMMAND_HANDLERS:
+    dispatcher.add_handler(handler)
 updater.start_polling()
--- commands.py
+++ commands.py
@@ -1,42 +1,164 @@
 import telegram
 from telegram.ext import CommandHandler
+from GelbooruViewer import GelbooruPicture, GelbooruViewer
+from random import randint
+from collections import defaultdict
+import pickle
+import atexit
+import signal
+import sys
+
+PICTURE_INFO_TEXT = """
+id: {picture_id}
+size: {width}*{height}
+tags: {tags}
+rating: {rating}
+"""
+PIC_CHAT_DIC_FILE_NAME = 'picture_chat_id.dic'
+gelbooru_viewer = GelbooruViewer()
+
+try:
+    with open(PIC_CHAT_DIC_FILE_NAME, 'rb') as fp:
+        picture_chat_id_dic = pickle.load(fp)
+except FileNotFoundError:
+    picture_chat_id_dic = defaultdict(set)
 
 
-def start(bot, update):
+@atexit.register
+def save_pic_chat_dic():
+    with open(PIC_CHAT_DIC_FILE_NAME, 'wb') as fp:
+        pickle.dump(picture_chat_id_dic, fp, protocol=2)
+
+
+def raise_exit(signum, stack):
+    sys.exit(-1)
+signal.signal(signal.SIGTERM, raise_exit)
+
+
+def hello(bot, update):
     bot.send_message(
         chat_id=update.message.chat_id,
-        text="My name is Altair, Archie Meng's partner"
+        text="""
+        My name is Altair, ArchieMeng's partner.
+        My core is shared on https://github.com/ArchieMeng/archie_partner_bot
+        """
     )
 
 
-def uppercase(bot: telegram.bot.Bot, update: telegram.Update, args):
-    bot.send_chat_action(telegram.ChatAction.TYPING)
+def send_gelbooru_images(bot: telegram.bot.Bot, update: telegram.Update, args):
+    chat_id = update.message.chat_id
+    message_id = update.message.message_id
+
+    def send_picture(p: GelbooruPicture):
+        bot.send_photo(
+            chat_id=chat_id,
+            reply_to_message_id=message_id,
+            photo=p.file_url
+        )
+
+    def send_picture_info(p: GelbooruPicture):
+        bot.send_message(chat_id=chat_id, text=PICTURE_INFO_TEXT.format(
+            picture_id=p.picture_id,
+            width=p.width,
+            height=p.height,
+            tags=' '.join(p.tags),
+            rating=p.rating
+        ))
+
+    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
 
     if args:
-        uppercase_args = ' '.join([arg.upper() for arg in args])
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text=uppercase_args
-        )
-    else:
-        help_text = '''
-            uppercase usage:
-                /upper <string>
-        '''
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text=help_text
-        )
+        # fetch picture_id = args[0] of it is digits
+        if args[0].isdigit():
+            picture = gelbooru_viewer.get(id=args[0])
+            if picture:
+                picture = picture[0]
+                send_picture(picture)
+                send_picture_info(picture)
+                picture_chat_id_dic[chat_id].add(picture.picture_id)
+            else:
+                bot.send_message(
+                    chat_id=chat_id,
+                    reply_to_message_id=message_id,
+                    text="id: {picture_id} not found".format(picture_id=args[0])
+                )
+            return
 
+        pictures = gelbooru_viewer.get(tags=args)
+        if len(pictures) >= 1:
+            for pic in pictures:
+                if pic.picture_id not in picture_chat_id_dic[chat_id]:
+                    send_picture(pic)
+                    send_picture_info(pic)
+                    picture_chat_id_dic[chat_id].add(pic.picture_id)
+                    break
+            else:
+                picture_chat_id_dic[chat_id] = {pictures[0].picture_id}
+                send_picture(pictures[0])
+                send_picture_info(pictures[0])
+        else:
+            bot.send_message(
+                chat_id=chat_id,
+                reply_to_message_id=message_id,
+                text="Tag: {tags} not found".format(tags=args)
+            )
+    else:
+        picture = gelbooru_viewer.get(limit=1)
+        while not picture or picture[0].picture_id in picture_chat_id_dic[chat_id]:
+            picture = gelbooru_viewer.get(id=randint(1, gelbooru_viewer.max_id))
+        picture = picture[0]
+        send_picture(picture)
+        send_picture_info(picture)
+        picture_chat_id_dic[chat_id].add(picture.picture_id)
+
+
+def source_id(bot: telegram.Bot, update: telegram.Update, args):
+    chat_id = update.message.chat_id
+    message_id = update.message.message_id
+
+    if args and args[0].isdigit():
+        pic_id = args[0]
+        picture = gelbooru_viewer.get(id=pic_id)
+        if picture:
+            picture = picture[0]
+            bot.send_message(
+                chat_id=chat_id,
+                reply_to_message_id=message_id,
+                text=picture.source
+            )
+        else:
+            bot.send_message(
+                chat_id=chat_id,
+                reply_to_message_id=message_id,
+                text="id: {pic_id} not found".format(pic_id=pic_id)
+            )
+    else:
+        bot.send_message(
+            chat_id=chat_id,
+            reply_to_message_id=message_id,
+            text="/source <id> to get source url of picture which has id.\n id must be an int"
+        )
 
 
 start_handler = CommandHandler(
     command='start',
-    callback=start
+    callback=hello
 )
-uppercase_handler = CommandHandler(
-    command='upper',
-    callback=uppercase,
+
+taxi_handler = CommandHandler(
+    command='taxi',
+    callback=send_gelbooru_images,
     pass_args=True
 )
+
+source_handler = CommandHandler(
+    command='source',
+    callback=source_id,
+    pass_args=True
+)
+COMMAND_HANDLERS = [
+    start_handler,
+    taxi_handler,
+    source_handler
+]
 i = 1
